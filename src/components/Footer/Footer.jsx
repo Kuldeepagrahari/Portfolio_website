b0affbb531b94a07557f8569b0ea6c29e4fddb88
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-12-15 01:08:19 +0530

    rsp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ import {Link} from "react-router-dom"
 const Footer = () => {
     return (
         <div className='footer'>
-            Follow me 
+            <h2>Follow me</h2> 
             <Link to="https://github.com/Kuldeepagrahari">
                 <FaGithubSquare style={{ boxShadow: "1px 1px 15px var(--theme-color)", border: "1px solid var(--theme-color)", fontSize: "30px" , color:"white"}} /></Link>  <Link to="">
                 <FaInstagramSquare style={{ boxShadow: "1px 1px 15px var(--theme-color)", border: "1px solid var(--theme-color)", fontSize: "30px", color:"white" }} /></Link>  <Link to=""><FaSquareXTwitter style={{ boxShadow: "1px 1px 15px var(--theme-color)", border: "1px solid var(--theme-color)", fontSize: "30px" , color:"white"}} /></Link>  <Link to="https://www.linkedin.com/in/kuldeep-agrahari-56b159260">
