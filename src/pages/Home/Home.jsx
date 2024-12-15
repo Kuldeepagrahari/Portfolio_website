c24f6414b3bf282112c4976871d205a604d28088
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-12-15 13:42:29 +0530

    styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <h2 style={{color:"gold"}}> Full Stack Developer </h2>
       <h1 className='head'>Hi, I'm <span style={{color:"var(--theme-color)"}}>Kuldeep</span></h1>
       <h1>Agrahari from UP, India</h1>
-      <button style={{border:"1px solid var(--theme-color)", backgroundColor:"transparent", color:"white", width:"150px", padding:"10px 10px", borderRadius:"10px", fontWeight:"bold",marginTop:"3vh", boxShadow:"1px 1px 30px var(--theme-color)"}}>Download my CV</button>
+      <button>Resume</button>
       </div>
       <div className="right">
        <img src="mypic.png" alt="pic" />
